8b2d3a1c9cef9cacf0a40df2a1d130fe8102ab2c
khoanna <khoa210105@gmail.com>
2025-12-04 17:18:16 +0700

    initialize project structure with configuration files and dotenv integration

---
+++ config/index.ts
@@ -0,0 +1,37 @@
+import "dotenv/config";
+import {
+  createZeroDevPaymasterClient,
+} from "@zerodev/sdk";
+import {http, createPublicClient, zeroAddress} from "viem";
+import {privateKeyToAccount} from "viem/accounts";
+import {sepolia} from "viem/chains";
+import {getEntryPoint, KERNEL_V3_1} from "@zerodev/sdk/constants";
+import type {Hex} from "viem";
+
+
+if (!process.env.ZERODEV_RPC) {
+  throw new Error("ZERODEV_RPC is not set");
+}
+
+if (!process.env.PRIVATE_KEY) {
+  throw new Error("PRIVATE_KEY is not set");
+}
+
+const signer = privateKeyToAccount(process.env.PRIVATE_KEY as Hex);
+
+const entryPoint = getEntryPoint("0.7");
+
+const kernelVersion = KERNEL_V3_1;
+
+const publicClient = createPublicClient({
+  transport: http(process.env.ZERODEV_RPC),
+  chain: sepolia,
+});
+
+// Deposit fund to your paymaster at dashboard.zerodev.app for sponsoring user operations
+const paymasterClient = createZeroDevPaymasterClient({
+  chain: sepolia,
+  transport: http(process.env.ZERODEV_RPC),
+});
+
+export {signer, entryPoint, kernelVersion, publicClient, paymasterClient};
---
+++ create-account/index.ts
@@ -0,0 +1,62 @@
+import "dotenv/config";
+import {
+  createKernelAccount,
+  createKernelAccountClient,
+} from "@zerodev/sdk";
+import {signerToEcdsaValidator} from "@zerodev/ecdsa-validator";
+import {http, zeroAddress} from "viem";
+import {sepolia} from "viem/chains";
+import {signer, entryPoint, kernelVersion, publicClient, paymasterClient} from "../config/index.ts";
+
+
+const main = async () => {
+  // === Create ECDSA Validator Plugin ===
+  const ecdsaValidator = await signerToEcdsaValidator(publicClient, {
+    signer,
+    entryPoint,
+    kernelVersion,
+  });
+
+  // === Create Kernel Account ===
+  const account = await createKernelAccount(publicClient, {
+    plugins: {
+      sudo: ecdsaValidator,
+    },
+    entryPoint,
+    kernelVersion,
+  });
+
+  // === Create Kernel Account Client ===
+  const kernelClient = createKernelAccountClient({
+    account,
+    chain: sepolia,
+    bundlerTransport: http(process.env.ZERODEV_RPC),
+    client: publicClient,
+    paymaster: {
+      getPaymasterData(userOperation) {
+        return paymasterClient.sponsorUserOperation({userOperation});
+      },
+    },
+  });
+
+  // === Send User Operation ===
+  const userOpHash = await kernelClient.sendUserOperation({
+    callData: await account.encodeCalls([
+      {
+        to: zeroAddress,
+        value: BigInt(0),
+        data: "0x",
+      },
+    ]),
+  });
+  console.log("userOp hash:", userOpHash);
+  const _receipt = await kernelClient.waitForUserOperationReceipt({
+    hash: userOpHash,
+  });
+  console.log("bundle txn hash: ", _receipt.receipt.transactionHash);
+  console.log("userOp completed");
+
+  process.exit(0);
+};
+
+main()
